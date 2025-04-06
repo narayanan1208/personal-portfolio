dff3fe81d4d05b932698231ed62b0c339cc6f377
narayanan1208 <narayanan6647@gmail.com>
2025-04-06 17:37:52 +0530

    Added react-vite app folders

---
+++ README.md
@@ -0,0 +1,2 @@
+# Personal portfolio
+Personal portfolio using ReactJS and TailwindCSS
---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./index.css";
+import "./App.css";
+import LoadingScreen from "./components/LoadingScreen";
+import Navbar from "./components/Navbar";
+import MobileMenu from "./components/MobileMenu";
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      {""}
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      >
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      </div>
+    </>
+  );
+}
+
+export default App;
