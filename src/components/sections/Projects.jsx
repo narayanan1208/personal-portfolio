f2e2aeb7022082af4097b0af278ee0573c5714e7
narayanan1208 <narayanan6647@gmail.com>
2025-04-08 23:17:06 +0530

    Updated Projects component

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -24,41 +24,7 @@ const Projects = () => {
                 modernised user interface.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["MongoDB", "Express.js", "ReactJS", "Node.js"].map(
-                  (tech, key) => (
-                    <span
-                      key={key}
-                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                    hover:shadow-[0_2px_8px_rgba(59,130,246,.1)] transition-all"
-                    >
-                      {tech}
-                    </span>
-                  )
-                )}
-              </div>
-
-              <div className="flex justify-between items-center">
-                <a
-                  href="#"
-                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
-                >
-                  {" "}
-                  View Projects →{" "}
-                </a>
-              </div>
-            </div>
-
-            {/* <div
-              className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 
-            hover:shadow-[0_2px_8px_rgba(59,130,246,.2)] transition-all"
-            >
-              <h3 className="text-xl font-bold mb-2">Food Delivery Website</h3>
-              <p className="text-gray-400 mb-4">
-                Scalable food delivery website with admin dashboard and
-                modernised user interface.
-              </p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                {["MongoDB", "Express.js", "ReactJS", "Node.js"].map(
+                {["MongoDB", "Express.js", "ReactJS", "Node.js", "JWT"].map(
                   (tech, key) => (
                     <span
                       key={key}
@@ -86,13 +52,84 @@ const Projects = () => {
               className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 
             hover:shadow-[0_2px_8px_rgba(59,130,246,.2)] transition-all"
             >
-              <h3 className="text-xl font-bold mb-2">Food Delivery Website</h3>
+              <h3 className="text-xl font-bold mb-2">
+                Student Management Portal
+              </h3>
               <p className="text-gray-400 mb-4">
-                Scalable food delivery website with admin dashboard and
-                modernised user interface.
+                Student management portal for school and college.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["MongoDB", "Express.js", "ReactJS", "Node.js"].map(
+                {[
+                  "Python",
+                  "Django",
+                  "ReactJS",
+                  "TypeScript",
+                  "JWT",
+                  "Bootstrap",
+                ].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                    hover:shadow-[0_2px_8px_rgba(59,130,246,.1)] transition-all"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  {" "}
+                  View Projects →{" "}
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 
+            hover:shadow-[0_2px_8px_rgba(59,130,246,.2)] transition-all"
+            >
+              <h3 className="text-xl font-bold mb-2">Paradise Nursery</h3>
+              <p className="text-gray-400 mb-4">
+                A simple user friendly plant shopping website
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["ReactJS", "Redux", "CSS"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                    hover:shadow-[0_2px_8px_rgba(59,130,246,.1)] transition-all"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  {" "}
+                  View Projects →{" "}
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 
+            hover:shadow-[0_2px_8px_rgba(59,130,246,.2)] transition-all"
+            >
+              <h3 className="text-xl font-bold mb-2">Bibliophile's Paradise</h3>
+              <p className="text-gray-400 mb-4">
+                A simple library checkout cystem where users can search and
+                borrow books from a library.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Python", "Flask", "ReactJS", "TypeScript", "Docker"].map(
                   (tech, key) => (
                     <span
                       key={key}
@@ -115,40 +152,6 @@ const Projects = () => {
                 </a>
               </div>
             </div>
-
-            <div
-              className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 
-            hover:shadow-[0_2px_8px_rgba(59,130,246,.2)] transition-all"
-            >
-              <h3 className="text-xl font-bold mb-2">Food Delivery Website</h3>
-              <p className="text-gray-400 mb-4">
-                Scalable food delivery website with admin dashboard and
-                modernised user interface.
-              </p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                {["MongoDB", "Express.js", "ReactJS", "Node.js"].map(
-                  (tech, key) => (
-                    <span
-                      key={key}
-                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                    hover:shadow-[0_2px_8px_rgba(59,130,246,.1)] transition-all"
-                    >
-                      {tech}
-                    </span>
-                  )
-                )}
-              </div>
-
-              <div className="flex justify-between items-center">
-                <a
-                  href="#"
-                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
-                >
-                  {" "}
-                  View Projects →{" "}
-                </a>
-              </div>
-            </div> */}
           </div>
         </div>
       </RevealOnScroll>
